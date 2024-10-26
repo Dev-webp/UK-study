85cf8c9e53118291567a3b575c23e790baa97fca
Dev-webp <srichain18@gmail.com>
2024-10-26 17:42:49 +0530

    G-ad

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,36 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Remarkable UK Study Visa: Fearless Steps To Take',
-  description: 'Get UK Student Visa - Discover essential details on UK study visa requirements, fees, and processing times for Indian students .CONTACT US FOR MORE DETAILS',
-}
+  description: 'Get UK Student Visa - Discover essential details on UK study visa requirements, fees, and processing times for Indian students. CONTACT US FOR MORE DETAILS',
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-587PFF4');`
+        }} />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-587PFF4"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
+        {/* End Google Tag Manager */}
         {children}
       </body>
     </html>
-  )
+  );
 }
